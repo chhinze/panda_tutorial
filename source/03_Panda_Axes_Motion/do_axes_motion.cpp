9c1fa7243eabc98a1010bcbfb3c1a2fb92b46b40
Christoph Hinze <christoph.hinze@isw.uni-stuttgart.de>
2019-07-05 13:18:29 +0200

    Add warning about motion to 03

--- source/03_Panda_Axes_Motion/do_axes_motion.cpp
+++ source/03_Panda_Axes_Motion/do_axes_motion.cpp
@@ -8,10 +8,18 @@ std::string pandaIP = "172.16.0.2";
 int main(void) {
   try {
     franka::Robot panda(pandaIP);
+    setDefaultBehavior(panda);
 
+    // 2. Define goal position and construct MotionGenerator (defined in "examples_common.h")
     std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
     MotionGenerator motion_generator(0.05, q_goal);
 
+    std::cout << "WARNING: The robot will move now. "
+              << "Keep around the STOP button." << std::endl
+              << "Press ENTER to continue." << std::endl;
+    std::cin.ignore();
+
+    // 3. start the control
     panda.control(motion_generator);
 
     std::cout << "Finished moving to joint configuration : [";
